4016c2b620255d018c6e47b829b4b05ce9cdff1c
sirapop <siapop46@gmail.com>
2021-06-30 09:30:47 +0700

    edited minor bugs

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 				<div class="jumbotron">
 					<h1 class="display-3">Make your own space.</h1>
 					<p class="lead">Make your own Blog, create your comfort zone. Post your progression whenever you want.</p>
-					<a class="btn btn-lg btn-primary" href="registration/register.php" role="button">Register &raquo;</a>
+					<a class="btn btn-lg btn-primary" href="register.php" role="button">Register &raquo;</a>
 				</div>
 			</div>
 		</div>
